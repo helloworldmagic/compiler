86c544bf3e3deea2d739c36186ece58ac36704a4
helloworldmagic <104927327+helloworldmagic@users.noreply.github.com>
2022-05-10 02:29:15 +0530

    Update program.cpp

--- exp10-intermidatetopostfix/program.cpp
+++ exp10-intermidatetopostfix/program.cpp
@@ -1,58 +1,69 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int preced(char ch) {
-   if(ch == '+' || ch == '-') return 1;          
-   else if(ch == '*' || ch == '/') return 2;         
-   else if(ch == '^') return 3;
-   return 0;
+int preced(char x){
+  if(x == '+' || x == '-') return 1;
+  else if(x == '*' || x== '/') return 2;
+  else if(x == '^') return 3;
+  return 0;
 }
 
-string inToPost(string intermidate) {
-   stack<char> s;
-   s.push('#');              
-   string postfix = "";     
+string intoPost(string infix){
+  stack<char> s;
+  s.push('#');
+  string postfix = "";
 
-   for(auto i: intermidate) {
-      if(isalnum(i))
-         postfix += i;
-      
-      else if(i == '(')
-         s.push('(');
-      
-      else if(i == '^')
-         s.push('^');
-        
-      else if(i == ')') {
-         while(s.top() != '#' && s.top() != '(') {
+  for(auto i: infix){
+    if(isalnum(i))
+      postfix += i;
+
+    else if(i=='(')
+      s.push('(');
+
+    else if(i=='^')
+      s.push('^');
+
+    else if(i==')'){
+      while(s.top()!='#' && s.top()!='('){
+        postfix+=s.top();
+        s.pop();
+      }
+      if(s.top()=='(')
+        s.pop();
+    }
+
+    else{
+      if(preced(i) > preced(s.top()))
+        s.push(i);
+      else{
+        while(s.top()!='#' && preced(i) <= preced(s.top())){
             postfix += s.top();
-            s.pop();
-         }
-         s.pop();
+           s.pop();
+        }
+        s.push(i);
       }
-        
-      else {
-         if(preced(i) > preced(s.top()))
-            s.push(i);
-         else {
-            while(s.top() != '#' && preced(i) <= preced(s.top())) {
-               postfix += s.top();      
-               s.pop();
-            }
-            s.push(i);
-         }
-      }
-   }
+    }
+    
+  }
 
-   while(s.top() != '#') {
+  while(s.top() != '#') {
       postfix += s.top();
       s.pop();
-   }
-
+  }
    return postfix;
 }
 
-int main() {
-   string intermidate = "x^y/(5*z)+2";
-   cout << "Postfix Form Is: "<< inToPost(intermidate)<<endl;
+int main(){
+  string infix = "a+b*(c^d-e)^(f+g*h)-i";
+  cout<<"Postfix form is : "<< intoPost(infix)<<endl;
+  reverse(infix.begin(), infix.end());
+
+  for(int i=0;i<infix.size();i++){
+    if(infix[i] == '(')
+      infix[i] = ')';
+    if(infix[i] == ')')
+      infix[i] = '(';
+  }
+
+  cout<<"Prefix form is : "<< intoPost(infix);
 }
